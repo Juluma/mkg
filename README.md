3e53b39e59177e7cf9d2b509e461ed8d1a195022
Juha Sarjanoja <juhasarjanoja@85-23-12-102.bb.dnainternet.fi>
2017-01-08 22:19:21 +0200

    js and css... 90% done

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ Morsiuskimppugeneraattori
 4.1.  1.25<br/>
 5.1.  1.50<br/>
 6.1.  1.00<br/>
-TOTAL 4.50
+7.1.  1.25<br/>
+8.1.  0.75<br/>
+TOTAL 6.5
 </pre>
--- js/app.js
+++ js/app.js
@@ -11,6 +11,12 @@ jQuery(document).ready(function($){
     };
     var images = [];
     var current = null;
+    var wpbs = 'xs';
+    if ($(window).width() >= 992) {
+        wpbs = 'md';
+    } else if ($(window).width() >= 768) {
+        wpbs = 'sm';
+    }
 
     makeSelectionBar = function(data) {
         var barHtml = '';
@@ -21,7 +27,11 @@ jQuery(document).ready(function($){
         });
 
         $.each(images, function(i, imgBase){
-            barHtml+= '<div class="col-xs-4 col-sm-3 col-md-2"><a href="#" rel="'+imgBase+'" class="bouquetPopup">';
+            var xStyle = '';
+            if (i >= 3 && wpbs == 'xs' || i >= 4 && wpbs == 'sm' || i >= 6 && wpbs == 'md') {
+                xStyle = ' style="display:none"';
+            }
+            barHtml+= '<div class="col-xs-4 col-sm-3 col-md-2"'+xStyle+'><a href="#" rel="'+imgBase+'" class="bouquetPopup">';
             barHtml+= '<img src="'+selImg.prefix+imgBase+selImg.postfix+'" class="img-responsive">';
             barHtml+= '</a></div>';
         });
@@ -41,39 +51,79 @@ jQuery(document).ready(function($){
 
     $('#selection-bar').on('click', '.bouquetPopup', function(e){
         e.preventDefault();
-        var imgBase = $(this).attr('rel');
-        var ladyHt = $('#lady').find('img:first').height();
-        var ladyOrigHt = $('#lady').find('img:first').prop('naturalHeight');
-        var ratio = ladyHt / ladyOrigHt * 2.5;
+        var imgBase     = $(this).attr('rel');
+        var divWth      = $('#lady').width();
+        var divHt       = $('#lady').height();
+        var ladyHt      = $('#lady').find('img:first').height();
+        var ladyOrigHt  = $('#lady').find('img:first').prop('naturalHeight');
+        var ratio       = ladyHt / ladyOrigHt * 2.5;
         current = imgBase;
 
         $('#selection-bar').find('div').removeClass('active');
         $('#lady').find('.over-image').remove();
+
+        // Add bouquet
         $(this).closest('div').addClass('active');
         var img = selImg.prefix + imgBase + selImg.postfix;
         $('#lady').append('<img src="'+img+'" class="over-image">');
-        $('#lady').find('img:last').height($('#lady').find('img:last').prop('naturalHeight') * ratio);
+        var bouquetImg = $('#lady').find('img:last');
+        bouquetImg.height($('#lady').find('img:last').prop('naturalHeight') * ratio);
+
+        var fix = 0;
+        // Place the bouquet correctly, when ratio between 0.25 - 0.5 (most usual cases)
+        // ... and correct there with other cases as well ... :)
+        var multiply = ratio * 10 - 2;
+        fix = multiply * -5;
+
+        // Position img by wrapping (relative) div
+        bouquetImg.css('top', divHt / 3.9);
+        bouquetImg.css('left', divWth / 2 + fix);
     });
 
     $('#arrow-right img').click(function(){
         var key = getArrayKeyByValue(current);
         var next = key + 1;
+        var link = null;
         if (next in images) {
-            $('#selection-bar').find('a[rel="'+images[next]+'"]').click();
+            link = $('#selection-bar').find('a[rel="'+images[next]+'"]');
         } else {
-            $('#selection-bar').find('a[rel="'+images[0]+'"]').click();
+            link = $('#selection-bar').find('a[rel="'+images[0]+'"]');
         }
+
+        setVisibilities(link, next, 'right');
+        link.click();
     });
 
     $('#arrow-left img').click(function(){
         var key = getArrayKeyByValue(current);
         var previous = key - 1;
+        var link = null;
         if (previous in images) {
-            $('#selection-bar').find('a[rel="'+images[previous]+'"]').click();
+            link = $('#selection-bar').find('a[rel="'+images[previous]+'"]');
         } else {
-            $('#selection-bar').find('a[rel="'+images[images.length-1]+'"]').click();
+            link = ('#selection-bar').find('a[rel="'+images[images.length-1]+'"]');
         }
+        setVisibilities(link, previous, 'left');
+        link.click();
     });
+
+    // Check if parent div is hidden and show that and correct ones
+    // based on viewport width (and hiding others)
+    setVisibilities = function(link, img, direction) {
+        if (link.closest('div').is(':visible')) {
+            return;
+        }
+        // Element is hidden; hide all scrollbar divs
+        $('#selection-bar > div').hide();
+
+        // Show correct ones based on viewport width, img and direction
+        var divCount = 3;
+        if (wpbs == 'sm') divCount = 4;
+        if (wpbs == 'md') divCount = 6;
+
+
+        console.log(img, direction);
+    }
 
     getArrayKeyByValue = function(value) {
         var key = null;
@@ -85,5 +135,9 @@ jQuery(document).ready(function($){
         });
         return key;
     }
+
+    $(window).resize(function(){
+        location.reload();
+    });
 
 });
